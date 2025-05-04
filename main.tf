001a3b844c7c9c80068bb4ac1e0a9de5fbd31745
Dean Chap <cchapotarong@mun.ca>
2025-05-04 00:09:51 -0600

    add emails, cicd test

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 }
 
 resource "aws_iam_role" "lambda_exec" {
-  name = "lambda_exec_role_v2"
+  name = "lambda_exec_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
